bca1198e4d16bf1c2193c2072e826c3451243d5c
mohazerro <mohazerro@github.com>
2022-11-14 15:03:32 +0100

    second

--- moni.sh
+++ moni.sh
@@ -17,8 +17,8 @@ diskusage=$(df -h --total | grep total | awk '{printf("%.1f/%.fGb (%s)\n", $3, $
 # UTILISATION CPU
 cpuload=$(top -bn1 | grep Cpu | awk '{printf("%.1f%%\n", $2 + $4)}')
 #top -bn1
-# -b affiche top par lots selon les linites de -n
-# 
+# -b affiche top par lots selon les limites de -n
+# printf additionne les colonnes du 2ieme arg et 4ieme arg
 # DERNIER REBOOT
 lastboot=$(uptime -s | awk '{printf("%s %s\n", $1, substr($2, 1, length($2) - 3))}')
 #uptime -s s pour since, depuis
@@ -36,17 +36,19 @@ userlog=$(users | wc -w)
 ip=$(cat /sys/class/net/enp3s0f0/address)
 host=$(hostname -I | awk '{print $1}')
 # NOMBRE DE COMMANDES SUDO
-journalctl _COMM=sudo | grep "COMMAND" | wc -l
-echo "#ARCHITECTURE: $arch"
-echo "#CPU physiques: $cpuphysique"
-echo "#CPU virtuels: $cpuvirtuel"
-echo "#Memory Usage: $memusage"
-echo "#Disk Usage: $diskusage"
-echo "#CPU load: $cpuload"
-echo "#Last boot: $lastboot"
-echo "#LVM use: $lvm"
-echo "#Connections TCP : $tcp"
-echo "#User log: $userlog"
-echo "#Network: IP $host $ip"
-echo "#Sudo : $sudo cmd"
+journalctl -q _COMM=sudo | grep "COMMAND" | wc -l
+# -q retire le message d'avertissement
+# _COMM permet de recuperer la commande desiree
+wall echo "#ARCHITECTURE: $arch
+#CPU physiques: $cpuphysique
+#CPU virtuels: $cpuvirtuel
+#Memory Usage: $memusage
+#Disk Usage: $diskusage
+#CPU load: $cpuload
+#Last boot: $lastboot
+#LVM use: $lvm
+#Connections TCP : $tcp
+#User log: $userlog
+#Network: IP $host ($ip)
+#Sudo : $sudo cmd"
 
